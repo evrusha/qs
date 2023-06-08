0b438f3f9855cef66298375a2855a616cb8c3f4a
Uladzislau Yeuhrafau <92786798+evrusha@users.noreply.github.com>
2023-06-08 09:17:23 +0000

    added_table

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,18 +1,12 @@
 class TransactionsController < ApplicationController
   def index
-    @transactions = Transaction.all
+    @transactions = Transaction.first(5)
   end
 
   def import
     incoming_file = params[:file]
     return redirect_to root_path unless incoming_file
 
-    dir = Rails.root.join('public/uploads')
-    Dir.mkdir(dir) unless Dir.exist?(dir)
-    file_name = "#{DateTime.now.strftime('%d%m%Y%H%M')}_file.csv"
-    File.open(dir.join(file_name), 'wb') do |file|
-      file.write(incoming_file.read)
-    end
-    CsvImportJob.perform_async(dir.join(file_name).to_s)
+    Transaction.import_csv(incoming_file)
   end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,11 @@
 class Transaction < ApplicationRecord
+  def self.import_csv(incoming_file)
+    dir = Rails.root.join('public/uploads')
+    Dir.mkdir(dir) unless Dir.exist?(dir)
+    file_name = "#{DateTime.now.strftime('%d%m%Y%H%M')}_file.csv"
+    File.open(dir.join(file_name), 'wb') do |file|
+      file.write(incoming_file.read)
+    end
+    CsvImportJob.perform_async(dir.join(file_name).to_s)
+  end
 end
